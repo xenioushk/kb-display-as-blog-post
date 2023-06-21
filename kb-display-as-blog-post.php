27facadf127452e9890d36526b57ae1bbde3b5a0
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2023-06-21 12:51:49 +0300

    Added gitlab.yml and fixed the parent plugin compatibility issue

--- kb-display-as-blog-post.php
+++ kb-display-as-blog-post.php
@@ -4,15 +4,14 @@
  * Plugin Name:    KB Display As Blog Post - BWL Knowledge Base Manager Addon
  * Plugin URI:        http://bit.ly/kb-as-blog
  * Description:      This is an Addon for BWL Knowledge Base Manager. It allows you to display your Knowledge Base contents as blog post. This Addon automatically include KB posts in you're blog listings according to date order. Addon comes with Quick and Bulk edit options, so you can easily choose which KB you want to show in blog lists. Addon also allows you to integrate 'featured image' with every KB.
- * Version:           1.0.5
+ * Version:           1.0.6
  * Author:             Md Mahbub Alam Khan
  * Author URI:      https://1.envato.market/xenioushk
  */
 // If this file is called directly, abort.
-if (!defined( 'WPINC' ) ) {
-    
-    die;
+if (!defined('WPINC')) {
 
+    die;
 }
 
 /* ----------------------------------------------------------------------------*
@@ -26,30 +25,29 @@ define('BKBDABP_PARENT_PLUGIN_INSTALLED_VERSION', get_option('bwl_kb_plugin_vers
 define('BKBDABP_ADDON_PARENT_PLUGIN_TITLE', 'BWL Knowledge Base Manager Plugin');
 define('BKBDABP_ADDON_TITLE', 'KB Display As Blog Post Addon');
 define('BKBDABP_PARENT_PLUGIN_REQUIRED_VERSION', '1.0.9'); // change plugin required version in here.
-define('BKBDABP_ADDON_CURRENT_VERSION', '1.0.5'); // change plugin current version in here.
+define('BKBDABP_ADDON_CURRENT_VERSION', '1.0.6'); // change plugin current version in here.
 
 define('BKBDABP_ADDON_PREFIX', 'bkb-kbdabp'); // Addon Data Prefix. It must be simmilar with $plugin slug (kb-display-as-blog-post\public\class-kbdabp-addon.php).
 
 define('BKBDABP_DIR', plugin_dir_path(__FILE__));
 
-require_once( plugin_dir_path( __FILE__ ) . 'public/class-kbdabp-addon.php' );
+require_once(plugin_dir_path(__FILE__) . 'public/class-kbdabp-addon.php');
 
 /*
  * Register hooks that are fired when the plugin is activated or deactivated.
  * When the plugin is deleted, the uninstall.php file is loaded.
  */
-register_activation_hook( __FILE__, array( 'BKB_kbdabp', 'activate' ) );
-register_deactivation_hook( __FILE__, array( 'BKB_kbdabp', 'deactivate' ) );
+register_activation_hook(__FILE__, array('BKB_kbdabp', 'activate'));
+register_deactivation_hook(__FILE__, array('BKB_kbdabp', 'deactivate'));
 
-add_action( 'plugins_loaded', array( 'BKB_kbdabp', 'get_instance' ) );
+add_action('plugins_loaded', array('BKB_kbdabp', 'get_instance'));
 
 /* ----------------------------------------------------------------------------*
  * Dashboard and Administrative Functionality
  * ---------------------------------------------------------------------------- */
 
-if ( is_admin()) {
+if (is_admin()) {
 
-    require_once( plugin_dir_path( __FILE__ ) . 'admin/class-kbdabp-addon-admin.php' );
-    add_action( 'plugins_loaded', array( 'BKB_kbdabp_Admin', 'get_instance' ) );
-
+    require_once(plugin_dir_path(__FILE__) . 'admin/class-kbdabp-addon-admin.php');
+    add_action('plugins_loaded', array('BKB_kbdabp_Admin', 'get_instance'));
 }
--- public/class-kbdabp-addon.php
+++ public/class-kbdabp-addon.php
@@ -1,6 +1,7 @@
 <?php
 
-class BKB_kbdabp {
+class BKB_kbdabp
+{
 
 	/**
 	 * Plugin version, used for cache-busting of style and script file references.
@@ -42,22 +43,20 @@ class BKB_kbdabp {
 	 *
 	 * @since     1.0.0
 	 */
-	private function __construct() {
-            
-                    if( class_exists( 'BWL_KB_Manager' ) && BKBDABP_PARENT_PLUGIN_REQUIRED_VERSION > '1.0.5' ) {
-                        
-                        add_filter( 'pre_get_posts', array( $this, 'bkb_kbdabp_filter_posts' ),10 ); 
-                        
-                        $bkb_data = get_option('bkb_options');
-                        
-                        if (isset($bkb_data['bkb_kb_blog_feed_status']) && $bkb_data['bkb_kb_blog_feed_status'] == 1) {
-                            
-                            add_filter('request', array( $this, 'bkb_kbdabp_include_feed' ));
-                            
-                        }
-                        
-                    }
+	private function __construct()
+	{
 
+		if (class_exists('BwlKbManager\\Init') && BKBDABP_PARENT_PLUGIN_REQUIRED_VERSION > '1.0.5') {
+
+			add_filter('pre_get_posts', array($this, 'bkb_kbdabp_filter_posts'), 10);
+
+			$bkb_data = get_option('bkb_options');
+
+			if (isset($bkb_data['bkb_kb_blog_feed_status']) && $bkb_data['bkb_kb_blog_feed_status'] == 1) {
+
+				add_filter('request', array($this, 'bkb_kbdabp_include_feed'));
+			}
+		}
 	}
 
 	/**
@@ -67,7 +66,8 @@ class BKB_kbdabp {
 	 *
 	 *@return    Plugin slug variable.
 	 */
-	public function get_plugin_slug() {
+	public function get_plugin_slug()
+	{
 		return $this->plugin_slug;
 	}
 
@@ -78,10 +78,11 @@ class BKB_kbdabp {
 	 *
 	 * @return    object    A single instance of this class.
 	 */
-	public static function get_instance() {
+	public static function get_instance()
+	{
 
 		// If the single instance hasn't been set, set it now.
-		if ( null == self::$instance ) {
+		if (null == self::$instance) {
 			self::$instance = new self;
 		}
 
@@ -98,31 +99,29 @@ class BKB_kbdabp {
 	 *                                       WPMU is disabled or plugin is
 	 *                                       activated on an individual blog.
 	 */
-	public static function activate( $network_wide ) {
+	public static function activate($network_wide)
+	{
 
-		if ( function_exists( 'is_multisite' ) && is_multisite() ) {
+		if (function_exists('is_multisite') && is_multisite()) {
 
-			if ( $network_wide  ) {
+			if ($network_wide) {
 
 				// Get all blog ids
 				$blog_ids = self::get_blog_ids();
 
-				foreach ( $blog_ids as $blog_id ) {
+				foreach ($blog_ids as $blog_id) {
 
-					switch_to_blog( $blog_id );
+					switch_to_blog($blog_id);
 					self::single_activate();
 				}
 
 				restore_current_blog();
-
 			} else {
 				self::single_activate();
 			}
-
 		} else {
 			self::single_activate();
 		}
-
 	}
 
 	/**
@@ -135,32 +134,29 @@ class BKB_kbdabp {
 	 *                                       WPMU is disabled or plugin is
 	 *                                       deactivated on an individual blog.
 	 */
-	public static function deactivate( $network_wide ) {
+	public static function deactivate($network_wide)
+	{
 
-		if ( function_exists( 'is_multisite' ) && is_multisite() ) {
+		if (function_exists('is_multisite') && is_multisite()) {
 
-			if ( $network_wide ) {
+			if ($network_wide) {
 
 				// Get all blog ids
 				$blog_ids = self::get_blog_ids();
 
-				foreach ( $blog_ids as $blog_id ) {
+				foreach ($blog_ids as $blog_id) {
 
-					switch_to_blog( $blog_id );
+					switch_to_blog($blog_id);
 					self::single_deactivate();
-
 				}
 
 				restore_current_blog();
-
 			} else {
 				self::single_deactivate();
 			}
-
 		} else {
 			self::single_deactivate();
 		}
-
 	}
 
 	/**
@@ -170,16 +166,16 @@ class BKB_kbdabp {
 	 *
 	 * @param    int    $blog_id    ID of the new blog.
 	 */
-	public function activate_new_site( $blog_id ) {
+	public function activate_new_site($blog_id)
+	{
 
-		if ( 1 !== did_action( 'wpmu_new_blog' ) ) {
+		if (1 !== did_action('wpmu_new_blog')) {
 			return;
 		}
 
-		switch_to_blog( $blog_id );
+		switch_to_blog($blog_id);
 		self::single_activate();
 		restore_current_blog();
-
 	}
 
 	/**
@@ -192,7 +188,8 @@ class BKB_kbdabp {
 	 *
 	 * @return   array|false    The blog ids, false if no matches.
 	 */
-	private static function get_blog_ids() {
+	private static function get_blog_ids()
+	{
 
 		global $wpdb;
 
@@ -201,8 +198,7 @@ class BKB_kbdabp {
 			WHERE archived = '0' AND spam = '0'
 			AND deleted = '0'";
 
-		return $wpdb->get_col( $sql );
-
+		return $wpdb->get_col($sql);
 	}
 
 	/**
@@ -210,7 +206,8 @@ class BKB_kbdabp {
 	 *
 	 * @since    1.0.0
 	 */
-	private static function single_activate() {
+	private static function single_activate()
+	{
 		// @TODO: Define activation functionality here
 	}
 
@@ -219,7 +216,8 @@ class BKB_kbdabp {
 	 *
 	 * @since    1.0.0
 	 */
-	private static function single_deactivate() {
+	private static function single_deactivate()
+	{
 		// @TODO: Define deactivation functionality here
 	}
 
@@ -228,63 +226,61 @@ class BKB_kbdabp {
 	 *
 	 * @since    1.0.0
 	 */
-	public function load_plugin_textdomain() {
+	public function load_plugin_textdomain()
+	{
 
 		$domain = $this->plugin_slug;
-		$locale = apply_filters( 'plugin_locale', get_locale(), $domain );
+		$locale = apply_filters('plugin_locale', get_locale(), $domain);
 
-		load_textdomain( $domain, trailingslashit( WP_LANG_DIR ) . $domain . '/' . $domain . '-' . $locale . '.mo' );
-
+		load_textdomain($domain, trailingslashit(WP_LANG_DIR) . $domain . '/' . $domain . '-' . $locale . '.mo');
 	}
-        
-                public function bkb_kbdabp_include_feed( $qv ) {
-                    if  (isset( $qv['feed'] ) && !isset( $qv['post_type'] ) )
-                        $qv['post_type'] = array('bwl_kb');
-                    return $qv;
-                }
-            
-              public function bkb_kbdabp_filter_posts($query) {
 
-                if ( is_home() && !is_admin() && !is_search() && $query->is_main_query() ) {
-                    
-                    $args = array(
-                            'post_status'       => 'publish',
-                            'post_type'         => 'bwl_kb',
-                            'ignore_sticky_posts' => 1,
-                            'posts_per_page' => -1
-                        );
-                    
-                    $args ['meta_query'] =  array(
-                        array(
-                         'key' => 'bkb_kbdabp_status',
-                         'compare' => '=',
-                         'value' => '1' 
-                        )
-                     );
-                    
-                    $loop = new WP_Query($args);
-                    
-                    $bkb_kbdabp_excluded_posts = array();
+	public function bkb_kbdabp_include_feed($qv)
+	{
+		if (isset($qv['feed']) && !isset($qv['post_type']))
+			$qv['post_type'] = array('bwl_kb');
+		return $qv;
+	}
 
-                    if (count($loop->posts) > 0) {
-                        
-                        foreach ($loop->posts as $posts) {
-                      
-                                $bkb_kbdabp_excluded_posts[] = $posts->ID;
-                            
-                        }
-                        
-                    }
-                    
-                wp_reset_query();
+	public function bkb_kbdabp_filter_posts($query)
+	{
 
-                $query->set('post_type', array('post', 'bwl_kb')); 
-                $query->set( 'post__not_in', apply_filters( 'bkb_rkb_blog_query_filter',$bkb_kbdabp_excluded_posts ) );  
-                return $query;
-                
-                } else {
-                    return $query;
-                }
-            }
+		if (is_home() && !is_admin() && !is_search() && $query->is_main_query()) {
 
+			$args = array(
+				'post_status'       => 'publish',
+				'post_type'         => 'bwl_kb',
+				'ignore_sticky_posts' => 1,
+				'posts_per_page' => -1
+			);
+
+			$args['meta_query'] =  array(
+				array(
+					'key' => 'bkb_kbdabp_status',
+					'compare' => '=',
+					'value' => '1'
+				)
+			);
+
+			$loop = new WP_Query($args);
+
+			$bkb_kbdabp_excluded_posts = array();
+
+			if (count($loop->posts) > 0) {
+
+				foreach ($loop->posts as $posts) {
+
+					$bkb_kbdabp_excluded_posts[] = $posts->ID;
+				}
+			}
+
+			wp_reset_query();
+
+			$query->set('post_type', array('post', 'bwl_kb'));
+			$query->set('post__not_in', apply_filters('bkb_rkb_blog_query_filter', $bkb_kbdabp_excluded_posts));
+			return $query;
+		} else {
+			return $query;
+		}
+	}
 }
