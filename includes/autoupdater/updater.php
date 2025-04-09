91d91dfa83a5c130b750b93fa465354f17ae2a50
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2025-04-09 16:15:10 +0300

    ❌ Removed: autoupdater old files.

--- includes/autoupdater/updater.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$updaterBase         = 'https://projects.bluewindlab.net/wpplugin/zipped/plugins/';
-$pluginRemoteUpdater = $updaterBase . 'bkbm/notifier_bkbm_dabp.php';
-new WpAutoUpdater( BKBDABP_ADDON_CURRENT_VERSION, $pluginRemoteUpdater, BKBDABP_ADDON_UPDATER_SLUG );
