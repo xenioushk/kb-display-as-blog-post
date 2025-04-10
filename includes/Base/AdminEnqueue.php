383fecf119c577ab33eac2e4d5950de7c54008e8
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2025-04-10 16:42:17 +0300

    ❌ Removed: Extra codes.

--- includes/Base/AdminEnqueue.php
+++ includes/Base/AdminEnqueue.php
@@ -36,13 +36,6 @@ class AdminEnqueue {
      */
 	public function get_the_scripts() {
 
-        // wp_enqueue_style(
-        //     $this->admin_script_slug,
-        //     KBDABP_PLUGIN_STYLES_ASSETS_DIR . 'admin.css',
-        //     [],
-        //     KBDABP_PLUGIN_VERSION
-        // );
-
         wp_enqueue_script(
             $this->admin_script_slug,
             KBDABP_PLUGIN_SCRIPTS_ASSETS_DIR . 'admin.js',
